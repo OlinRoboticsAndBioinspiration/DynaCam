91f7ca9ae1fcdd2edb658e098e57c53ca2d77254
Claire Keum <ayoung.keum@students.olin.edu>
2014-03-24 15:22:01 -0400

    Changed SRC Address

--- dynaroach/src/network.h
+++ dynaroach/src/network.h
@@ -4,6 +4,6 @@
 #define NETWORK_BASESTATION_CHANNEL     0x14
 #define NETWORK_BASESTATION_PAN_ID      0x0000
 #define NETWORK_BASESTATION_ADDR        0x0101
-#define NETWORK_SRC_ADDR                0x2000
+#define NETWORK_SRC_ADDR                0x2001
 
 #endif
